c5dd970276fb9b4bf883c611185616551f53329e
linxiaoyuan <linxiaoyuan@bytedance.com>
2020-06-04 11:46:57 +0800

    add comment

--- 01-bootsector-barebones/boot_sect_simple.asm
+++ 01-bootsector-barebones/boot_sect_simple.asm
@@ -2,5 +2,10 @@
 loop:
     jmp loop
 
+;NASM supports two special tokens in expressions, allowing calculations to involve the current assembly position: 
+;the $ and $$ tokens. $ evaluates to the assembly position at the beginning of the line containing the expression; 
+;so you can code an infinite loop using JMP $. $$ evaluates to the beginning of the current section; 
+;so you can tell how far into the section you are by using ($-$$).
+
 times 510-($-$$) db 0
 dw 0xaa55
