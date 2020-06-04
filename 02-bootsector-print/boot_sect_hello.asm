e775e0d734ea1ee276b7117e2602f88801f60765
linxiaoyuan <linxiaoyuan@bytedance.com>
2020-06-04 11:56:29 +0800

    modify output

--- 02-bootsector-print/boot_sect_hello.asm
+++ 02-bootsector-print/boot_sect_hello.asm
@@ -1,14 +1,19 @@
 mov ah, 0x0e ; tty mode
-mov al, 'H'
+mov al, 'G'
 int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10 ; 'l' is still on al, remember?
 mov al, 'o'
 int 0x10
-
+int 0x10
+mov al, 'd'
+int 0x10 ; 'l' is still on al, remember?
+mov al, ' '
+int 0x10
+mov al, 'J'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'b'
+int 0x10
 jmp $ ; jump to current address = infinite loop
 
 ; padding and magic number
