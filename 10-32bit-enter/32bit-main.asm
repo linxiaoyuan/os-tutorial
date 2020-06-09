4af7ff9259c60fac8187b8d6278eccee39419324
linxiaoyuan <linxiaoyuan@bytedance.com>
2020-06-09 16:49:00 +0800

    finish 10

--- 10-32bit-enter/32bit-main.asm
+++ 10-32bit-enter/32bit-main.asm
@@ -21,6 +21,7 @@ BEGIN_PM: ; after the switch we will get here
 
 MSG_REAL_MODE db "Started in 16-bit real mode", 0
 MSG_PROT_MODE db "Loaded 32-bit protected mode", 0
+MSG_TEST db "test aaa",0
 
 ; bootsector
 times 510-($-$$) db 0
--- 10-32bit-enter/32bit-switch.asm
+++ 10-32bit-enter/32bit-switch.asm
@@ -1,10 +1,13 @@
 [bits 16]
 switch_to_pm:
+    mov bx, MSG_TEST
+    call print 
     cli ; 1. disable interrupts
     lgdt [gdt_descriptor] ; 2. load the GDT descriptor
     mov eax, cr0
     or eax, 0x1 ; 3. set 32-bit mode bit in cr0
     mov cr0, eax
+
     jmp CODE_SEG:init_pm ; 4. far jump by using a different segment
 
 [bits 32]
